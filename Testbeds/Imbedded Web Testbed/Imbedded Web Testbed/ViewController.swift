60452c669f7dbc2d322f9efe317a97b7e9e5cc6a
Charlie Imhoff <imhoffc@carleton.edu>
2015-05-08 14:23:36 -0500

    added a WKWebView via coded constraints and the same problem occurs; webpages are drawn with the frame size of the whole iPad, and then clipped by the view.

--- Testbeds/Imbedded Web Testbed/Imbedded Web Testbed/ViewController.swift
+++ Testbeds/Imbedded Web Testbed/Imbedded Web Testbed/ViewController.swift
@@ -13,16 +13,24 @@ class ViewController: UIViewController {
 
 	var webView : WKWebView!
 	
-	override func loadView() {
-		super.loadView()
-		
-		webView = WKWebView()
-		//constraints: middle align, top, leading, bottom margins
-		
-	}
-	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		webView = WKWebView()
+		self.view.addSubview(webView)
+		
+		webView.setTranslatesAutoresizingMaskIntoConstraints(false)
+		let viewsDictionary : [NSObject:AnyObject] = ["webView":self.webView, "top":self.topLayoutGuide, "bottom":self.bottomLayoutGuide]
+		
+		//constraints: middle align, top, leading, bottom margins
+		let webViewHorizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
+			"H:|-250-[webView]-250-|", options: NSLayoutFormatOptions.allZeros, metrics: nil, views: viewsDictionary)
+		let webViewVerticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:[top]-250-[webView]-250-|", options: NSLayoutFormatOptions.allZeros, metrics: nil, views: viewsDictionary)
+		let webViewCenterConstraint = NSLayoutConstraint(item: webView, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.CenterX, multiplier: 1, constant: 0)
+		self.view.addConstraints(webViewHorizontalConstraints)
+		self.view.addConstraints(webViewVerticalConstraints)
+		self.view.addConstraint(webViewCenterConstraint)
+		
 		
 		let url = NSURL(string: "http://www.google.com")
 		let request = NSURLRequest(URL: url!)
